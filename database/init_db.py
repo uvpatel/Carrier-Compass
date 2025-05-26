4166149b3955675720367a13e52951f6314c8a67
uvpatel <uvpatel7271@gmail.com>
2025-05-26 12:24:57 +0530

    added some

--- database/init_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('database/career_compass.db')
-c = conn.cursor()
-with open('database/schema.sql') as f:
-    c.executescript(f.read())
-conn.commit()
-conn.close()
-print("Database initialized!")
